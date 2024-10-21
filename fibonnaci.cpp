cbb9b6d908e4fb9b9975cdbcd98e7eaf2202f9b6
ahmadismail764 <ahmedeldeehy30305@gmail.com>
2024-10-21 09:16:37 +0300

    Question1-Part1

--- fibonnaci.cpp
+++ fibonnaci.cpp
@@ -1,6 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int recursive_fib(int n)
+{
+    return ((n <= 1) ? recursive_fib(n - 1) + recursive_fib(n - 2) : n);
+}
 int main()
 {
     cout << "Hello world\n";
