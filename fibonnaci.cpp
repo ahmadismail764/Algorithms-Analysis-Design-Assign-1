93a9237b62d1563571bffae93ec856356dc759fa
ahmadismail764 <ahmedeldeehy30305@gmail.com>
2024-10-21 09:18:17 +0300

    Readme added + fib adjusted

--- fibonnaci.cpp
+++ fibonnaci.cpp
@@ -1,10 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int recursive_fib(int n)
-{
-    return ((n <= 1) ? recursive_fib(n - 1) + recursive_fib(n - 2) : n);
-}
+int recursive_fib(int n) { return ((n <= 1) ? recursive_fib(n - 1) + recursive_fib(n - 2) : n); }
+
 int main()
 {
     cout << "Hello world\n";
